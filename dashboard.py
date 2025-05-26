43990b25bed1564d5e02aff7875975953fb96d56
unknown <daniel.barberoj@gmail.com>
2025-05-26 14:34:23 +0200

    Primeros Datamarts en un Dashboard funcional

--- dashboard.py
+++ dashboard.py
@@ -6,9 +6,123 @@ from src.utils.db import get_connection
 
 con = get_connection()
 
-st.title("🌦️ MeteoPanda Dashboard")
+st.set_page_config(
+    page_title="MeteoPanda Dashboard",
+    page_icon="🌦️",
+
+    layout="wide"
+)
+
+st.title("🐼 MeteoPanda Dashboard")
 
 df = con.execute("SELECT * FROM gold.city_yearly_summary").df()
 
-city = st.selectbox("Ciudad", df['city'].unique())
-st.line_chart(df[df['city'] == city].set_index('year')[['avg_temp', 'total_precip']])
+
+with st.sidebar:
+    #Filtro de año
+    year = st.selectbox("Año", options=df['year'].unique(), index=0)
+    #Filtro de Mes
+    month = st.selectbox("Mes", options=sorted(df['month'].unique()), index=0)
+    #Filtro de Region
+    region = st.selectbox("Region", options=df['region'].unique(), index=0)
+    #Filtro de Ciudades
+    city = st.multiselect("Ciudad", options=df['city'].unique())
+
+
+#Aplicamos los filtros en cascada
+if year:
+    df_filtered = df[df['year'] == year]
+    if month:
+        df_filtered = df_filtered[df_filtered['month'] <= month]
+    if region:
+        df_filtered = df_filtered[df_filtered['region'] == region]
+    if city:
+        df_filtered = df_filtered[df_filtered['city'].isin(city)]
+
+
+
+st.header(f"Resumen de {month}/{year}")
+
+#Metricas
+cols = st.columns(len(df_filtered['city'].unique()))
+
+# Calculate metrics
+metrics = {
+    "Temperatura Promedio": "avg_temp",
+    "Temperatura Máxima": "max_temp", 
+    "Temperatura Mínima": "min_temp",
+    "Precipitación Total": "total_precip",
+    "Tiempo de Sol": "total_sunshine"
+}
+
+# Display metrics for each city in columns
+for i, city_name in enumerate(df_filtered['city'].unique()):
+    city_data = df_filtered[df_filtered['city'] == city_name]
+    
+    with cols[i]:
+        st.subheader(city_name.capitalize())
+        for metric_name, metric_col in metrics.items():
+            value = city_data[metric_col].iloc[0]
+            if pd.isna(value):
+                st.metric(label=metric_name, value="Sin datos")
+                if metric_name == "Precipitación Total":
+                    st.progress(0)
+            else:
+                if metric_name == "Precipitación Total":
+                  
+                    # Mostrar el valor 
+                    st.metric(label=f"{metric_name}", value=f"{round(value, 2)} mm")
+                    
+                    # Mostrar barra de progreso
+                    max_precip = 100 
+                    st.progress(min(value/max_precip, 1.0))
+                else:
+                    st.metric(label=metric_name, value=round(value, 2))
+
+
+st.header(f"Dias extremos de {month}/{year}")
+
+# Crear nuevas columnas para días extremos
+num_cities = len(df_filtered['city'].unique())
+    
+df_extreme = con.execute("SELECT * FROM gold.city_extreme_days").df()
+extreme_cols = st.columns(num_cities)
+
+if year:
+    df_filtered = df_extreme[df_extreme['year'] == year]
+    if month:
+        df_filtered = df_filtered[df_filtered['month'] <= month]
+    if region:
+        df_filtered = df_filtered[df_filtered['region'] == region]
+    if city:
+        df_filtered = df_filtered[df_filtered['city'].isin(city)]
+
+extreme_metrics = {
+    "Fecha": "date",
+    "Temperatura Máxima": "temp_max_c", 
+    "Temperatura Mínima": "temp_min_c",
+    "Precipitación Total": "precip_mm"
+}
+
+# Display metrics for each city in columns
+for i, city_name in enumerate(df_filtered['city'].unique()):
+    city_data = df_filtered[df_filtered['city'] == city_name]
+    
+    with extreme_cols[i]:
+        st.subheader(city_name.capitalize())
+        for metric_name, metric_col in extreme_metrics.items():
+            value = city_data[metric_col].iloc[0]
+            if pd.isna(value):
+                st.metric(label=metric_name, value="Sin datos")
+            else:
+                if metric_name == "Fecha":
+                    # Convertir la fecha a formato string
+                    value = value.strftime('%d/%m/%Y')
+                if metric_name == "Precipitación Total":
+                    # Mostrar el valor 
+                    st.metric(label=f"{metric_name}", value=f"{round(value, 2)} mm")
+                    
+                    # Mostrar barra de progreso
+                    max_precip = 100 
+                    st.progress(min(value/max_precip, 1.0))
+                st.metric(label=metric_name, value=value)
--- src/transform/sql/datamarts/basics_datamarts.sql
+++ src/transform/sql/datamarts/basics_datamarts.sql
@@ -5,6 +5,7 @@ SELECT
     city,
     region,
     year,
+    strftime('%m', date) AS month,
     COUNT(*) AS days,
     ROUND(AVG(temp_avg_c), 2) AS avg_temp,
     ROUND(MAX(temp_max_c), 2) AS max_temp,
@@ -12,10 +13,10 @@ SELECT
     ROUND(SUM(precip_mm), 2) AS total_precip,
     ROUND(SUM(sunshine_min), 2) AS total_sunshine
 FROM silver.weather_cleaned
-GROUP BY city, region, year;
+GROUP BY city, region, year, month;
 
 CREATE OR REPLACE TABLE gold.city_extreme_days AS
-SELECT *
+SELECT *, strftime('%m', date) AS month
 FROM silver.weather_cleaned
 WHERE
-    temp_max_c > 40 OR temp_min_c < 0 OR precip_mm > 50 OR sunshine_min > 1000;
+    temp_max_c > 40 OR temp_min_c < 0 OR precip_mm > 50
